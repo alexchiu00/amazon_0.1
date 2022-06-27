1cba75f7037cff1581d8c89eef4c662429a48ce0
alexchiu00 <alexchiu.t@gmail.com>
2022-06-27 21:09:49 +0800

    fix serverSideFetch error

--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -1,23 +1,12 @@
 import { getSession, useSession } from "next-auth/react";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import db from "../../firebase";
-import {
-  collection,
-  Firestore,
-  getDocs,
-  orderBy,
-  query,
-} from "firebase/firestore/lite";
+import { collection, getDocs, orderBy, query } from "firebase/firestore/lite";
 import Header from "../components/Header";
 import moment from "moment";
 import Order from "../components/Order";
-import { Orders, OrderType } from "../types/type";
 
-const Orders = ({ orders }: Orders) => {
-  const [ordersRendering, setOrders] = useState(Array<OrderType>);
-  useEffect(() => {
-    setOrders(orders);
-  }, []);
+const Orders = ({ orders }) => {
   const session = useSession();
   return (
     <div>
@@ -27,12 +16,12 @@ const Orders = ({ orders }: Orders) => {
           Your Orders
         </h1>
         {session.status === "authenticated" ? (
-          <h2>{ordersRendering.length} Orders</h2>
+          <h2>{orders.length} Orders</h2>
         ) : (
           <h2>Please sign in to see your orders</h2>
         )}
         <div className="mt-5 space-y-4">
-          {ordersRendering?.map((order) => (
+          {orders?.map((order) => (
             <Order order={order} key={order.id} />
           ))}
         </div>
@@ -41,33 +30,39 @@ const Orders = ({ orders }: Orders) => {
   );
 };
 
-export async function getServerSideProps(context: any) {
+export async function getServerSideProps(context) {
   const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
   const session = await getSession(context);
 
-  async function getOrders(db: Firestore, email: any) {
+  async function getOrders(db, email) {
     const ordersColRef = collection(db, "users", email, "orders");
     const q = query(ordersColRef, orderBy("timestamp", "desc"));
     const ordersList = await getDocs(q);
     return ordersList.docs;
   }
+  var orders;
+  try {
+    const stripeOrders = await getOrders(db, session?.user?.email);
+    const orderFromDB = await Promise.all(
+      stripeOrders.map(async (order) => ({
+        id: order.id,
+        amount: order.data().amount,
+        amount_shipping: order.data().amount_shipping,
+        images: order.data().images,
+        timestamp: moment(order.data().timestamp.toDate()).unix(),
+        items: (
+          await stripe.checkout.sessions.listLineItems(order.id, {
+            limit: 1000,
+          })
+        ).data,
+      }))
+    );
+    orders = orderFromDB;
+  } catch (error) {
+    console.log(error);
+  }
 
-  const stripeOrders = await getOrders(db, session?.user?.email);
-  const orders = await Promise.all(
-    stripeOrders.map(async (order) => ({
-      id: order.id,
-      amount: order.data().amount,
-      amount_shipping: order.data().amount_shipping,
-      images: order.data().images,
-      timestamp: moment(order.data().timestamp.toDate()).unix(),
-      items: (
-        await stripe.checkout.sessions.listLineItems(order.id, {
-          limit: 1000,
-        })
-      ).data,
-    }))
-  );
-  if (!session) {
+  if (!session?.user) {
     return {
       props: {},
     };
