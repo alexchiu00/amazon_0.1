6f85ab68c3c879446a141d935af79829c97f410e
alexchiu00 <alexchiu.t@gmail.com>
2022-06-27 22:18:51 +0800

    add public to stripe public key

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   env: {
-    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+    stripe_public_key: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     stripe_secret_key: process.env.STRIPE_SECRET_KEY,
   },
 };
