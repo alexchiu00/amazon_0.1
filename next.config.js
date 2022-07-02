3defd1e55ceff208478ce1e55a68c4542fc70304
alexchiu00 <alexchiu.t@gmail.com>
2022-07-02 23:10:53 +0800

    add public to sk var

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   },
   env: {
     stripe_public_key: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-    stripe_secret_key: process.env.STRIPE_SECRET_KEY,
+    stripe_secret_key: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
     stripe_signing_secret: process.env.STRIPE_SIGNING_SECRET,
   },
 };
