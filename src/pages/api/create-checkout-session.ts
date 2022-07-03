0ca0083ca0ec5dd3abe5f4b47d6f30372d7f8515
alexchiu00 <alexchiu.t@gmail.com>
2022-07-03 17:04:40 +0800

    comment out checkout code for testing

--- src/pages/api/create-checkout-session.ts
+++ src/pages/api/create-checkout-session.ts
@@ -3,18 +3,18 @@ const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 export default async (req: any, res: any) => {
   const { items, email } = req.body;
 
-  const transformedItems = items.map((item: any) => ({
-    quantity: 1,
-    price_data: {
-      currency: "hkd",
-      unit_amount: item.price * 100,
-      product_data: {
-        name: item.title,
-        images: [item.image],
-        description: item.description,
-      },
-    },
-  }));
+  // const transformedItems = await items.map((item: any) => ({
+  //   quantity: 1,
+  //   description: item.description,
+  //   price_data: {
+  //     currency: "hkd",
+  //     unit_amount: item.price * 100,
+  //     product_data: {
+  //       name: item.title,
+  //       images: [item.image],
+  //     },
+  //   },
+  // }));
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
@@ -22,15 +22,16 @@ export default async (req: any, res: any) => {
       shipping_address_collection: {
         allowed_countries: ["HK", "US", "CA"],
       },
-      line_items: transformedItems,
+      // line_items: transformedItems,
+      line_items: [{ price: "price_H5ggYwtDq4fbrJ", quantity: 2 }],
       mode: "payment",
 
       success_url: `${process.env.NEXT_PUBLIC_HOST}/success`,
       cancel_url: `${process.env.NEXT_PUBLIC_HOST}/checkout`,
-      metadata: {
-        email,
-        images: JSON.stringify(items.map((item: any) => item.image)),
-      },
+      // metadata: {
+      //   email,
+      //   images: JSON.stringify(items.map((item: any) => item.image)),
+      // },
     });
     res.status(200).json({ id: session.id });
   } catch (error) {
