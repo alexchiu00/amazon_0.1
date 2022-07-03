6ebf60b0fc532ad200597d2910f0b70ad2f56e9d
alexchiu00 <alexchiu.t@gmail.com>
2022-07-03 17:10:23 +0800

    hard code stripe sk for testing

--- src/pages/api/create-checkout-session.ts
+++ src/pages/api/create-checkout-session.ts
@@ -1,20 +1,22 @@
-const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = require("stripe")(
+  "sk_test_51LDg6mIRV64cFDkXNf7bUdvdwmWfeZcOyBoYILHw68IlZMmbCurq1d7DFyBA5FtWRNV3u5kO8bwphskhsxAprddc00Jrbg8py5"
+);
 
 export default async (req: any, res: any) => {
   const { items, email } = req.body;
 
-  // const transformedItems = await items.map((item: any) => ({
-  //   quantity: 1,
-  //   description: item.description,
-  //   price_data: {
-  //     currency: "hkd",
-  //     unit_amount: item.price * 100,
-  //     product_data: {
-  //       name: item.title,
-  //       images: [item.image],
-  //     },
-  //   },
-  // }));
+  const transformedItems = await items.map((item: any) => ({
+    quantity: 1,
+    description: item.description,
+    price_data: {
+      currency: "hkd",
+      unit_amount: item.price * 100,
+      product_data: {
+        name: item.title,
+        images: [item.image],
+      },
+    },
+  }));
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
@@ -22,16 +24,15 @@ export default async (req: any, res: any) => {
       shipping_address_collection: {
         allowed_countries: ["HK", "US", "CA"],
       },
-      // line_items: transformedItems,
-      line_items: [{ price: "price_H5ggYwtDq4fbrJ", quantity: 2 }],
+      line_items: transformedItems,
       mode: "payment",
 
       success_url: `${process.env.NEXT_PUBLIC_HOST}/success`,
       cancel_url: `${process.env.NEXT_PUBLIC_HOST}/checkout`,
-      // metadata: {
-      //   email,
-      //   images: JSON.stringify(items.map((item: any) => item.image)),
-      // },
+      metadata: {
+        email,
+        images: JSON.stringify(items.map((item: any) => item.image)),
+      },
     });
     res.status(200).json({ id: session.id });
   } catch (error) {
