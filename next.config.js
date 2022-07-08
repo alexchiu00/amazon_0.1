8981c73368be0ac5ad14660db7d4a397c20cf1f0
alexchiu00 <alexchiu.t@gmail.com>
2022-07-08 11:57:39 +0800

    try to fix .env issue

--- next.config.js
+++ next.config.js
@@ -8,13 +8,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   env: {
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-    NEXT_PUBLIC_STRIPE_SECRET_KEY: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
-    STRIPE_SIGNING_SECRET: process.env.STRIPE_SIGNING_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    GOOGLE_ID: process.env.GOOGLE_ID,
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
   },
 };
 
